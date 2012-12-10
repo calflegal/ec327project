e48bf8c48f6933d3b5112b60828c2fa30142c40f
Andy Mo <andy2m11@gmail.com>
2012-12-09 23:05:11 -0500

    completed all direction cases

--- jni/FixBoard.cpp
+++ jni/FixBoard.cpp
@@ -5,14 +5,29 @@ extern "C"{
 
 void fixBoard(int c, int r,  int** board, int p)
 {
-
 if(canConquerUp(  c,  r, p, board ))
-{
-	Conquer( c,  r, 0, -1,  board, p);
-}
+{	Conquer( c,  r, 0, -1,  board, p);	}
 
+if(canConquerDown(  c,  r, p, board ))
+{	Conquer( c,  r, 0, 1,  board, p);	}
 
+if(canConquerLeft(  c,  r, p, board ))
+{	Conquer( c,  r, -1, 0,  board, p);	}
 
+if(canConquerRight(  c,  r, p, board ))
+{	Conquer( c,  r, -1, 0,  board, p);	}
+
+if(canConquerDL(  c,  r, p, board ))
+{	Conquer( c,  r, -1, 1,  board, p);	}
+
+if(canConquerDR(  c,  r, p, board ))
+{	Conquer( c,  r, 1, 1,  board, p);	}
+
+if(canConquerUL(  c,  r, p, board ))
+{	Conquer( c,  r, -1, -1,  board, p);	}
+
+if(canConquerUR(  c,  r, p, board ))
+{	Conquer( c,  r, 1, -1,  board, p);	}
 }
 
 
--- jni/IsMoveAllowed.cpp
+++ jni/IsMoveAllowed.cpp
@@ -8,12 +8,16 @@ using namespace std;
 //Jboolean Java_com_ec327cassio_reversi_MainActivity_isValid(JNIEnv * env, jobject obj, jint c, jint r, int* board, int p ))
 
 bool isValid( int c, int r, int** board, int p) {
-		cout << board[5][3] << endl;
              bool canPut;
-			bool is;
-			bool cu;
 
-             canPut = isEmpty(c,r,board) && ( (canConquerUp(c, r, p, board)));
+             canPut = isEmpty(c,r,board) && ( (canConquerUp(c, r, p, board))	||
+            		 	 	 	 	 	 	  (canConquerDown(c, r, p, board))	||
+            		 	 	 	 	 	 	  (canConquerLeft(c, r, p, board))	||
+            		 	 	 	 	 	 	  (canConquerRight(c, r, p, board)) ||
+            		 	 	 	 	 	 	  (canConquerDL(c, r, p, board))	||
+            		 	 	 	 	 	 	  (canConquerDR(c, r, p, board))	||
+            		 	 	 	 	 	 	  (canConquerUL(c, r, p, board)) 	||
+            		 	 	 	 	 	 	  (canConquerUR(c, r, p, board))    );
             		 	 	 	 	 	 
 
              return canPut;
