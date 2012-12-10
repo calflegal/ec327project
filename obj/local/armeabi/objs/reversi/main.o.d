bda038a20522e039d5b05a26b4dd97046589ca4f
Andy Mo <andy2m11@gmail.com>
2012-12-10 13:38:00 -0500

    Still fixing ndk stuff

--- obj/local/armeabi/objs/reversi/main.o.d
+++ obj/local/armeabi/objs/reversi/main.o.d
@@ -1,16 +1,7 @@
 obj/local/armeabi/objs/reversi/main.o: jni/main.cpp \
- /Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/jni.h \
- /Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/string.h \
- /Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
- /Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
- /Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/malloc.h
+ /cygdrive/c/users/andy/downloads/android-ndk-r8c-windows/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/jni.h \
+ jni/gamelogic.h
 
-/Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/jni.h:
+/cygdrive/c/users/andy/downloads/android-ndk-r8c-windows/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/jni.h:
 
-/Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/string.h:
-
-/Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
-
-/Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
-
-/Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/malloc.h:
+jni/gamelogic.h:
