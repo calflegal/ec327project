b95221bddfb3ed0fb5a99d2cfe14ace8aa305d90
Calvin Flegal <calvin.flegal@gmail.com>
2012-12-10 23:25:37 -0500

    Up to lastest

--- obj/local/armeabi/objs/reversi/main.o.d
+++ obj/local/armeabi/objs/reversi/main.o.d
@@ -1,7 +1,10 @@
 obj/local/armeabi/objs/reversi/main.o: jni/main.cpp \
  /Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/jni.h \
+ jni/gamelogic.h \
  /Users/calvinflegal/Developer/android-ndk-r8c/sources/cxx-stl/system/include/cstddef
 
 /Users/calvinflegal/Developer/android-ndk-r8c/platforms/android-14/arch-arm/usr/include/jni.h:
+
+jni/gamelogic.h:
 
 /Users/calvinflegal/Developer/android-ndk-r8c/sources/cxx-stl/system/include/cstddef:
